fb2f71677ce15e5a08541725757e5df07fd50985
KoiShin <koikeshinnosuke0415@gmail.com>
2015-12-01 13:23:00 +0900

    create check_subtracter function

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int check_adder() {
     int actual, expected;
 
     srandom(time(NULL));
+    clear_by_zero(&num_result);
 
     for (i = 0; i < loop_count; i++) {
         int a = random() % 10000000;
@@ -31,7 +32,46 @@ int check_adder() {
     return 0;
 }
 
+int check_subtracter() {
+    int i;
+    Number num, num2, num_result;
+    int loop_count = 100000;
+    int actual, expected;
+
+    clear_by_zero(&num_result);
+
+    for (i = 0; i < loop_count; i++) {
+        int a = random() % 10000000;
+        int b = random() % 10000000;
+
+        set_int(&num,  a);
+        set_int(&num2, b);
+        // printf("a        = %d\n", a);
+        // printf("b        = %d\n", b);
+
+        expected = a - b;
+        // printf("expected = %d\n", expected);
+
+        subtract(&num, &num2, &num_result);
+        // display_number(&num_result);
+        // putchar('\n');
+
+        get_int(&num_result, &actual);
+        // printf("actual   = %d\n", actual);
+
+        if (actual != expected) {
+            puts("NG!!");
+        }
+        // putchar('\n');
+    }
+
+    puts("OK.");
+    return 0;
+}
+
 int main(void) {
-    check_adder();
+    srandom(time(NULL));
+    check_subtracter();
+
     return 0;
 }
