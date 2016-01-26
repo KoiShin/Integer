0e4723aefafb3d5e3894867616b0606013b968db
KoiShin <koikeshinnosuke0415@gmail.com>
2016-01-26 13:34:41 +0900

    create power() and fix bugs

--- c/integer.h
+++ c/integer.h
@@ -33,7 +33,10 @@ int  subtract(const Number*, const Number*, Number*);
 int  increment(const Number*, Number*);
 int  decrement(const Number*, Number*);
 int  simple_multiple(int, int, int*);
+int  multiple_positive_num(const Number*, const Number*, Number*);
 int  multiple(const Number*, const Number*, Number*);
+int  divide(const Number*, const Number*, Number*, Number*);
+int  power(const Number*, const Number*, Number*);
 
 void clear_by_zero(Number *num) {
     int i;
@@ -80,6 +83,8 @@ void set_random_number(Number *num, int digit_number) {
 
 void copy_number(const Number *from_num, Number *to_num) {
     int i;
+    clear_by_zero(to_num);
+
     for (i = 0; i < DIGIT_NUMBER; i++) {
         to_num->n[i] = from_num->n[i];
     }
@@ -87,6 +92,8 @@ void copy_number(const Number *from_num, Number *to_num) {
 }
 
 void get_abs(const Number *num, Number *abs_num) {
+    clear_by_zero(abs_num);
+
     copy_number(num, abs_num);
     set_sign(abs_num, 1);
 }
@@ -101,6 +108,8 @@ int is_zero(const Number *num) {
 
 int multiply_by_ten(const Number *num, Number *computation) {
     int i;
+
+    clear_by_zero(computation);
 
     if (num->n[DIGIT_NUMBER - 1] != 0) {
         puts("overflow!!");
@@ -117,6 +126,8 @@ int multiply_by_ten(const Number *num, Number *computation) {
 
 int divided_by_ten(const Number *num, Number *computation) {
     int i, surplus;
+
+    clear_by_zero(computation);
 
     if (num->n[DIGIT_NUMBER - 1] != 0) {
         puts("underflow!!");
@@ -143,15 +154,18 @@ int set_int(Number *num, long x) {
     int i;
 
     clear_by_zero(num);
-    if (x < 0) {
-        set_sign(num, -1);
-        x *= -1;
-    }
 
     for (i = 0; i < DIGIT_NUMBER; i++) {
         if (i >= DIGIT_NUMBER) return -1;
         num->n[i] = x % 10;
         x /= 10;
+    }
+
+    if (x >= 0) {
+        set_sign(num, 1);
+    } else {
+        set_sign(num, -1);
+        x *= -1;
     }
 
     return 0;
@@ -164,7 +178,7 @@ int get_int(const Number *num, int *x) {
     for (i = 0; i < DIGIT_NUMBER; i++) {
         *x += num->n[i] * pow(10, i);
     }
-    if (num->sign == -1) {
+    if (get_sign(num) == -1) {
         *x *= -1;
     }
     return 0;
@@ -292,6 +306,7 @@ int subtract(const Number *num, const Number *num2, Number *result) {
 int increment(const Number *num, Number *result) {
     Number one;
     set_int(&one, 1);
+    clear_by_zero(result);
 
     return add(num, &one, result);
 }
@@ -299,6 +314,7 @@ int increment(const Number *num, Number *result) {
 int decrement(const Number *num, Number *result) {
     Number one;
     set_int(&one, 1);
+    clear_by_zero(result);
 
     return subtract(num, &one, result);
 }
@@ -322,13 +338,15 @@ int multiple_positive_num(const Number *num, const Number *num2, Number *result)
     int num_i, num2_i;
     int carry = 0;
     int r;
-    Number tmp, tmp2;
+    Number result_, tmp_result;
     clear_by_zero(result);
-    clear_by_zero(&tmp);
+    clear_by_zero(&result_);
+    clear_by_zero(&tmp_result);
 
     for (num2_i = 0; num2_i < DIGIT_NUMBER - 1; num2_i++) {
         Number tmp3, tmp4;
         clear_by_zero(&tmp3);
+        clear_by_zero(&tmp4);
 
         for (num_i = 0; num_i < DIGIT_NUMBER - 1; num_i++) {
             int mul = num->n[num_i] * num2->n[num2_i] + carry;
@@ -342,12 +360,12 @@ int multiple_positive_num(const Number *num, const Number *num2, Number *result)
             copy_number(&tmp4, &tmp3);
         }
 
-        r = add(&tmp, &tmp3, &tmp2);
+        r = add(&result_, &tmp3, &tmp_result);
         if (r != 0) return -1;
-        copy_number(&tmp2, &tmp);
+        copy_number(&tmp_result, &result_);
     }
 
-    copy_number(&tmp, result);
+    copy_number(&result_, result);
 
     return 0;
 }
@@ -364,6 +382,7 @@ int multiple(const Number *num, const Number *num2, Number *result) {
 
     if (positive_num && positive_num2) {
         r = multiple_positive_num(num, num2, result);
+        set_sign(result, 1);
     } else if (positive_num && !positive_num2) {
         r = multiple_positive_num(num, &abs_num2, result);
         set_sign(result, -1);
@@ -372,6 +391,7 @@ int multiple(const Number *num, const Number *num2, Number *result) {
         set_sign(result, -1);
     } else if (!positive_num && !positive_num2) {
         r = multiple_positive_num(&abs_num, &abs_num2, result);
+        set_sign(result, 1);
     } else {
         return -1;
     }
@@ -388,6 +408,8 @@ int divide(const Number *dividend, const Number *divisor, Number *result, Number
 
     clear_by_zero(result);
     clear_by_zero(surplus);
+    clear_by_zero(&tmp);
+    clear_by_zero(&dividend_);
 
     if (is_zero(divisor) == 0) return -1;
 
@@ -399,6 +421,28 @@ int divide(const Number *dividend, const Number *divisor, Number *result, Number
         subtract(&dividend_, divisor, &tmp); copy_number(&tmp, &dividend_);
     }
     copy_number(&dividend_, surplus);
+
+    return 0;
+}
+
+int power(const Number *base, const Number *exponent, Number *result) {
+    int i = 0;
+    int exponent_;
+    Number result_;
+
+    get_int(exponent, &exponent_);
+    set_int(result, 1);
+    clear_by_zero(&result_);
+
+    if (exponent_ <  0) return -2;
+    if (exponent_ == 0) return  0;
+
+    while (1) {
+        if (i >= exponent_) break;
+        multiple(result, base, &result_);
+        copy_number(&result_, result);
+        i++;
+    }
 
     return 0;
 }
