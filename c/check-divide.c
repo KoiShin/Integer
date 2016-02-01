21b4a45fd246778ce97786d0a427064473b9ed70
KoiShin <koikeshinnosuke0415@gmail.com>
2016-02-01 15:16:32 +0900

    prevent divide of 0

--- c/check-divide.c
+++ c/check-divide.c
@@ -4,7 +4,7 @@
 int main(void) {
     int i;
     Number num, num2, num_result, num_surplus;
-    int loop_count = 1000;
+    int loop_count = 10000;
     int actual_result, actual_surplus;
     int expected_result, expected_surplus;
 
@@ -12,17 +12,17 @@ int main(void) {
 
     for (i = 0; i < loop_count; i++) {
         int a = random() % 1000;
-        int b = random() % 1000;
+        int b = random() % 1000 + 1;
 
-        // a *= (random() % 2 == 0) ? 1: -1;
-        // b *= (random() % 2 == 0) ? 1: -1;
+        a *= (random() % 2 == 0) ? 1: -1;
+        b *= (random() % 2 == 0) ? 1: -1;
         set_int(&num,  a);
         set_int(&num2, b);
 
         expected_result = a / b;
         expected_surplus = a % b;
 
-        divide(&num, &num2, &num_result, &num_surplus);
+        fast_divide(&num, &num2, &num_result, &num_surplus);
         get_int(&num_result, &actual_result);
         get_int(&num_surplus, &actual_surplus);
 
